10b2ac72c5b4029eef0fd90d09470fe57d2aadd8
farideh8217 <95141123+farideh8217@users.noreply.github.com>
2022-03-16 14:01:12 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "hello world";
+echo "hello world";
+echo "<br>"; 
+echo "hello world";
+echo "<br>";
+echo "hello world";
+echo "<br>";
+echo "hello world";
+echo "<br>";
+
+
+echo "heloo" . "    ".$x;
+#echo "farideh";
+echo '<p style="color:red">'.'salam'.' '.'khobi'.'</p>';
+$x="people";
+print_r($x); 
+echo "<br>";
+var_dump($x);
+
+$txt = 14;
+echo $txt;
+
+const url="7learn.com";
+echo "<hr>";
+echo url;
+define("BASEURL","7learn.com");
+echo BASEURL;
