56712efd8d330318df8d9aa145bcb791ed1520ce
farideh zaker <farideh.zaker8217@yahoo.com>
2022-04-17 17:16:06 +0430

    js

--- 13-pdo/4-select.php
+++ 13-pdo/4-select.php
@@ -1,2 +1,30 @@
 <?php
-include "1-connection.php";
+include_once "1-connection.php";
+
+function echopeople($row) 
+{
+    foreach($row as $col) 
+    {
+        echo "$col -";
+    }
+    echo PHP_EOL; // echo implode("-",$row).PHP_EOL; می توانیم از این تابع هم استفاده کنیم
+}
+function echopeoples($rows) 
+{
+    foreach($rows as $row) 
+    {
+        echopeople($row);
+    }
+}
+$sql="select * from people where age < 40";
+$stmt=$db->prepare($sql);
+$stmt->execute();
+//fetchall
+// $rows=$stmt->fetchAll(PDO::FETCH_OBJ);
+// echopeoples($rows);
+
+
+while ($row=$stmt->fetch(PDO::FETCH_OBJ)){
+    echo("$row->fullname :$row->age");
+}
+
